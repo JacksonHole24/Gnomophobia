80f189f9fd3ebe91b6f842e622ce59dd1d8555b5
AIE\s210837 <s210837@students.aie.edu.au>
2023-06-19 13:50:38 +0930

    Idk what changed

--- Gnome/Assets/Scripts/GnomeObject.cs
+++ Gnome/Assets/Scripts/GnomeObject.cs
@@ -4,14 +4,9 @@ using UnityEngine;
 
 public class GnomeObject : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [HideInInspector] public Gnome gnome;
 
-    // Update is called once per frame
-    void Update()
+    public void Break()
     {
         
     }
